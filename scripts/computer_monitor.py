7c5a32bbe8b6c647966e05f88f637b429b154a5e
OSU UWRT <osu.uwrt@gmail.com>
2021-02-16 18:47:21 -0500

    Fixing bugs

--- scripts/computer_monitor.py
+++ scripts/computer_monitor.py
@@ -1,38 +1,4 @@
 #!/usr/bin/env python
-#
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2017, TNO IVS, Helmond, Netherlands
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of the TNO IVS nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
-# \author Rein Appeldoorn
 
 import socket
 import psutil
@@ -55,15 +21,12 @@ class CpuTask(DiagnosticTask):
 
         stat.add("CPU Load Average", "{:.2f}".format(cpu_average))
 
-        warn = False
         for idx, val in enumerate(cpu_percentages):
             stat.add("CPU {} Load".format(idx), "{:.2f}".format(val))
-            if val > self._warning_percentage:
-                warn = True
 
-        if warn:
+        if cpu_average > self._warning_percentage:
             stat.summary(DiagnosticStatus.WARN,
-                         "At least one CPU exceeds {:d}%".format(self._warning_percentage))
+                         "Average CPU usage exceeds {:d}%".format(self._warning_percentage))
         else:
             stat.summary(DiagnosticStatus.OK, "Average CPU utilization {:.2f}%".format(cpu_average))
 
@@ -184,7 +147,7 @@ class GPUTask(DiagnosticTask):
             total = int(values[3].replace(" MiB", ""))
             util = int(values[4].replace(" %", ""))
 
-            stat.add("GPU {:d} ({:s}) Memory".format(id, name), "{:d} MB / {:d} MB ({:.2f}%)".format(used, total, float(used) / total))
+            stat.add("GPU {:d} ({:s}) Memory".format(id, name), "{:d} MB / {:d} MB ({:.2f}%)".format(used, total, 100.0 * used / total))
             stat.add("GPU {:d} ({:s}) Utilization".format(id, name), "{:d}%".format(util))
             gpu_utils.append(util)
 
