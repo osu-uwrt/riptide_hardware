1397b8be8a34e461d6a14b16c96104c45194d2ba
mitchsayre <sketch0306@gmail.com>
2022-09-02 15:13:40 -0400

    switch to vectornav IMU

--- launch/imu.launch.py
+++ launch/imu.launch.py
@@ -1,32 +1,65 @@
-import launch
 from launch.launch_description import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.launch_description_sources import AnyLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, LogInfo
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import LaunchConfiguration as LC
-
+from launch.substitutions import TextSubstitution as TS
 import os
 
-imu_launch_file = os.path.join(
-    get_package_share_directory('microstrain_inertial_driver'),
-    "launch", "microstrain_launch.py"
+imu_config_file = os.path.join(
+    get_package_share_directory('riptide_hardware2'),
+    "cfg", "vectornav_imu_config.yaml"
 )
 
-def generate_launch_description():
-    return LaunchDescription([
-        DeclareLaunchArgument('robot', default_value="tempest", description="Name of the vehicle"),
-        DeclareLaunchArgument('serial_port', default_value="/dev/microstrain"),
+# Vectornav
+start_vectornav_cmd = Node(
+    package='vectornav', 
+    executable='vectornav',
+    output='screen',
+    parameters=[
+        imu_config_file,
+        {
+            'frame_id': LC("frame_id"),
+            'port': LC("serial_port")
+        }
+    ],
+    namespace=LC("robot")
+)
 
-        IncludeLaunchDescription(
-            AnyLaunchDescriptionSource(imu_launch_file),
-            launch_arguments=[
-                ('params_file', os.path.join(get_package_share_directory('riptide_hardware2'), "cfg", "imu_config.yaml")),
-                ('namespace', ['/', LC('robot'), '/imu']),
-                ('configure', 'true'),
-                ('activate', 'true'),
-                ('port', LC('serial_port')),
-                ('imu_frame_id', [LC('robot'), '/imu_link']),
-            ]
-        )
+start_vectornav_sensor_msgs_cmd = Node(
+    package='vectornav', 
+    executable='vn_sensor_msgs',
+    output='screen',
+    parameters=[
+        imu_config_file,
+    ],
+    # this makes it compatible with the microstrain driver for a bit
+    remappings=[
+        ([TS(text="/"), LC("robot"), TS(text="/vectornav/imu")], [TS(text="/"), LC("robot"), TS(text="/imu/imu/data")])
+    ],
+    namespace=LC("robot")
+)
+
+robot_arg = DeclareLaunchArgument('robot', default_value="tempest", description="Name of the vehicle")
+serial_arg = DeclareLaunchArgument('serial_port', default_value="/dev/imu_vector")
+frame_id_arg = DeclareLaunchArgument('frame_id', default_value=[LC('robot'), "/imu_link"])
+
+before_topic_remap = DeclareLaunchArgument('serial_port', default_value="/dev/imu_vector")
+before_topic_remap = DeclareLaunchArgument('serial_port', default_value="/dev/imu_vector")
+
+def generate_launch_description():
+    # Create the launch description and populate
+    return LaunchDescription([
+        # launch args
+        robot_arg,
+        serial_arg,
+        frame_id_arg,
+        
+        # print the startup info
+        # LogInfo(msg=LC("frame_id")),
+        LogInfo(msg=["Serial port: ", LC("serial_port")]),
+        
+        # start the nodes
+        start_vectornav_cmd,
+        start_vectornav_sensor_msgs_cmd
     ])
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch*')),
-        (os.path.join('share', package_name, 'cfg'), glob('cfg/**'))
+        (os.path.join('share', package_name, 'cfg'), glob('cfg/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
